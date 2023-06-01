999d76cec3ab0a8f9f0d83c1e107c0a48827c00e
김진우 <jinwoo3661@naver.com>
2023-06-01 23:57:29 +0900

    init main, fragment

--- app/src/main/java/com/example/myfooddiarybookaos/MainActivity.kt
+++ app/src/main/java/com/example/myfooddiarybookaos/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myfooddiarybookaos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myfooddiarybookaos.TabHome.HomeFragment
+import com.example.myfooddiarybookaos.TabMyAccount.MyFragment
+import com.example.myfooddiarybookaos.TabSearch.SearchFragment
+import com.example.myfooddiarybookaos.TabTimeLine.TimeLineFragment
+import com.example.myfooddiarybookaos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    // 프래그먼트 생성
+    var homeFragment : HomeFragment? = null
+    var myFragment : MyFragment? = null
+    var searchFragment : SearchFragment? = null
+    var timeLineFragment : TimeLineFragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
