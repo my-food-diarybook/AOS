923db8a3ae879879ee0d4a8e081b6e0d694e12c4
김진우 <jinwoo3661@naver.com>
2023-06-06 12:11:51 +0900

    fragment ui 구성, icon, 설정값 추가 , 슬라이드 메뉴 추가

--- app/src/main/java/com/example/myfooddiarybookaos/MainActivity.kt
+++ app/src/main/java/com/example/myfooddiarybookaos/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.myfooddiarybookaos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.example.myfooddiarybookaos.Dialog.SelectImageFragment
 import com.example.myfooddiarybookaos.TabHome.HomeFragment
 import com.example.myfooddiarybookaos.TabMyAccount.MyFragment
 import com.example.myfooddiarybookaos.TabSearch.SearchFragment
 import com.example.myfooddiarybookaos.TabTimeLine.TimeLineFragment
 import com.example.myfooddiarybookaos.databinding.ActivityMainBinding
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -57,6 +60,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        // 클릭 이벤트
+        setUpListener()
     }
 
     private fun initFragment(){
@@ -66,5 +72,30 @@ class MainActivity : AppCompatActivity() {
         timeLineFragment = TimeLineFragment()
 
         transaction!!.add(R.id.main_frame,homeFragment!!).commit()
+    }
+
+    private fun setSubFragment(newFrag : Fragment){
+        fragmentManager?.beginTransaction()
+            ?.replace(R.id.slide_layout,newFrag)
+            ?.addToBackStack(null)
+            ?.commit()
+        val state =  binding.frameLayout.panelState
+        // 닫힌 상태일 경우 열기
+        if (state == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+            binding.frameLayout.panelState = SlidingUpPanelLayout.PanelState.ANCHORED
+        }
+        // 열린 상태일 경우 닫기
+        else if (state == SlidingUpPanelLayout.PanelState.EXPANDED) {
+            binding.frameLayout.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED
+            transaction
+                ?.remove(newFrag)
+                ?.commit()
+        }
+    }
+
+    private fun setUpListener(){
+        binding.addButton.setOnClickListener {
+            setSubFragment(SelectImageFragment())
+        }
     }
 }
