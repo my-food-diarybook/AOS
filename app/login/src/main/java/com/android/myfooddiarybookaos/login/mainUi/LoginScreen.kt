a79938d1f7248dcc151d26d2bf030c1269a8f8b1
jinuemong <jinwoo3661@naver.com>
2023-09-12 18:49:34 +0900

    ui fix add password view edittext fix

--- app/login/src/main/java/com/android/myfooddiarybookaos/login/mainUi/LoginScreen.kt
+++ app/login/src/main/java/com/android/myfooddiarybookaos/login/mainUi/LoginScreen.kt
@@ -1,8 +1,28 @@
 package com.android.myfooddiarybookaos.login.mainUi
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.android.myfooddiarybookaos.login.mainSubUi.BottomLayout
+import com.android.myfooddiarybookaos.login.mainSubUi.MidLayout
+import com.android.myfooddiarybookaos.login.mainSubUi.TopLayout
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(navController : NavHostController) {
+    // 뒤로가기 제어
+    BackHandler(enabled = true, onBack = {})
 
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        TopLayout()
+        MidLayout()
+        BottomLayout(
+            findPassword = { navController.navigate("findPassScreen") }
+        )
+    }
 }
