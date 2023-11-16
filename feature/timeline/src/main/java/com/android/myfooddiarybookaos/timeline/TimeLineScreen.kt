98632edfd98545d93df77f91bd83f12380f1a568
jinuemong <jinwoo3661@naver.com>
2023-11-16 18:05:26 +0900

    Feat: timeline 레포 생성, 데이터 불러오기 기능 구현

--- feature/timeline/src/main/java/com/android/myfooddiarybookaos/timeline/TimeLineScreen.kt
+++ feature/timeline/src/main/java/com/android/myfooddiarybookaos/timeline/TimeLineScreen.kt
@@ -2,12 +2,14 @@ package com.android.myfooddiarybookaos.TabTimeLine
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.android.myfooddiarybookaos.Layout.NotDataView
 import com.android.myfooddiarybookaos.Layout.TopCalendarLayout
 import com.android.myfooddiarybookaos.data.dataCalendar.viewModel.TodayViewModel
+import com.android.myfooddiarybookaos.timeline.ui.LoadTimeLineScreen
 import com.android.myfooddiarybookaos.timeline.viewModel.TimeLineViewModel
 
 @Composable
@@ -15,15 +17,21 @@ fun TimeLineScreen(
     todayViewModel: TodayViewModel = hiltViewModel(),
     timeLineViewModel: TimeLineViewModel = hiltViewModel()
 ) {
+
+    val timeLineData = timeLineViewModel.timeLine.observeAsState().value
+
+    LaunchedEffect(Unit){
+        timeLineViewModel.setTimeLineData(todayViewModel.getTodayDate())
+    }
+
     Column {
         // 캘린더 초기화
         TopCalendarLayout(todayViewModel)
 
-        val timeLineData = timeLineViewModel.timeLine.observeAsState().value
         if (timeLineData == null || timeLineData.isEmpty()) {
             NotDataView()
         } else {
-
+            LoadTimeLineScreen(timeLineData)
         }
     }
 }
--- feature/timeline/src/main/java/com/android/myfooddiarybookaos/timeline/item/TimeLineItem.kt
+++ feature/timeline/src/main/java/com/android/myfooddiarybookaos/timeline/item/TimeLineItem.kt
@@ -1,42 +1,67 @@
 package com.android.myfooddiarybookaos.timeline.item
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.android.myfooddiarybookaos.core.data.R
+import com.android.myfooddiarybookaos.data.dataCalendar.viewModel.TodayViewModel
 import com.android.myfooddiarybookaos.model.timeLine.TimeLine
 
 @Composable
 fun TimeLineItem(
+    todayViewModel: TodayViewModel = hiltViewModel(),
     timeLine: TimeLine,
-    images: List<String>,
     screenWidth: Dp
 ) {
+
     val dataSize = timeLine.diaryList.size
     val imageSize = remember { mutableStateOf(getImageDp(dataSize, screenWidth)) }
 
-    LazyRow(
-        modifier = Modifier.fillMaxSize(),
-        state = rememberLazyListState()
-    ) {
-        items(images) { image ->
-            ImageItem(
-                bytes = image,
-                imageSize = imageSize.value,
-                onClick = {
-                    // go data
-                }
-            )
+    val currentFont =
+        if (todayViewModel.getTodayDate() == timeLine.date)
+            FontFamily(Font(R.font.roboto_regular, FontWeight.W700)
+        )
+        else FontFamily(Font(R.font.roboto_regular, FontWeight.W400))
+
+    Column {
+        Text(
+            modifier = Modifier.padding(start = 20.dp, bottom = 2.dp, top = 12.dp),
+            text = todayViewModel.getTopDate(timeLine.date),
+            fontFamily = currentFont,
+            color = Color.Black
+        )
+
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(134.dp),
+            state = rememberLazyListState()
+        ) {
+            items(timeLine.diaryList) { diary ->
+                ImageItem(
+                    bytes = diary.bytes,
+                    imageSize = imageSize.value,
+                    onClick = {
+                        // go data
+                    }
+                )
+            }
         }
     }
 
