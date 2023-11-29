6ab81a3158c9b784e3ad477f9f39ce96feb2287e
jinuemong <jinwoo3661@naver.com>
2023-11-30 01:33:07 +0900

    [Feat/Gallery]: 갤러리 ui 생성, 패키지 구조화

---
+++ feature/detail/src/main/java/com/android/myfooddiarybookaos/detail/galleryUi/ui/GalleryScreen.kt
@@ -0,0 +1,13 @@
+package com.android.myfooddiarybookaos.detail.galleryUi.ui
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.android.myfooddiarybookaos.detail.viewModel.DetailViewModel
+
+@Composable
+fun GalleryScreen(
+    viewModel: DetailViewModel = hiltViewModel()
+) {
+
+
+}
