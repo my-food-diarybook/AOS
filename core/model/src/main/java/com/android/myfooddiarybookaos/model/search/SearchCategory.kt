f0d414f0a4630e257e25620b62c876bc13d8eee6
jinuemong <jinwoo3661@naver.com>
2023-12-29 12:22:49 +0900

    [Feat]: 검색 모델 기능 구현

---
+++ core/model/src/main/java/com/android/myfooddiarybookaos/model/search/SearchCategory.kt
@@ -0,0 +1,10 @@
+package com.android.myfooddiarybookaos.model.search
+
+import java.io.Serializable
+
+data class SearchCategory(
+    val categoryName: String,
+    val categoryType: String,
+    val count: Int,
+    val diaryList: List<SearchDiary>
+): Serializable
