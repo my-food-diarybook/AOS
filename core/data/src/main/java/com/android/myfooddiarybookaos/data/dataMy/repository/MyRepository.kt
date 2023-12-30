553e6e7dd92e48a77444a2938811fe4508c4bf06
jinuemong <jinwoo3661@naver.com>
2023-12-31 00:27:31 +0900

    [Feat]: 뷰와 데이터 바인딩

--- core/data/src/main/java/com/android/myfooddiarybookaos/data/dataMy/repository/MyRepository.kt
+++ core/data/src/main/java/com/android/myfooddiarybookaos/data/dataMy/repository/MyRepository.kt
@@ -1,10 +1,20 @@
 package com.android.myfooddiarybookaos.data.dataMy.repository
 
 import com.android.myfooddiarybookaos.api.NetworkManager
+import com.android.myfooddiarybookaos.model.statistics.StatisticsList
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class MyRepository @Inject constructor(
-    private val networkManager: NetworkManager
+    networkManager: NetworkManager
 ) {
+    private val manager = networkManager.getMyApiService()
+
+    suspend fun getUserStatistics() : Flow<StatisticsList> = flow {
+        try {
+            emit(manager.getUserStatistics())
+        } catch (_: Exception){ }
+    }
 
 }
--- feature/myaccount/src/main/java/com/android/myfooddiarybookaos/myaccount/viewModel/MyViewModel.kt
+++ feature/myaccount/src/main/java/com/android/myfooddiarybookaos/myaccount/viewModel/MyViewModel.kt
@@ -4,17 +4,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import com.android.myfooddiarybookaos.api.myApi.NoticeEntity
+import com.android.myfooddiarybookaos.data.dataMy.repository.MyRepository
 import com.android.myfooddiarybookaos.data.dataMy.repository.NoticeRepository
+import com.android.myfooddiarybookaos.model.statistics.StatisticsList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyViewModel @Inject constructor(
-    private val noticeRepository: NoticeRepository
+    private val noticeRepository: NoticeRepository,
+    private val myRepository: MyRepository
 ): ViewModel(){
 
     private val _noticeList =
@@ -22,11 +26,25 @@ class MyViewModel @Inject constructor(
     var noticeList: Flow<PagingData<NoticeEntity>> =
         _noticeList.asStateFlow()
 
+    private val _userStatistics =
+        MutableStateFlow<StatisticsList>(StatisticsList(listOf(),0))
+    private val userStatistics: Flow<StatisticsList> =
+        _userStatistics.asStateFlow()
+
     init {
+        getStatistics()
         getNotice()
     }
 
     private fun getNotice() = viewModelScope.launch {
-        noticeList = noticeRepository.getNoticePager()
+        noticeRepository.getNoticePager().collectLatest {
+            _noticeList.value = it
+        }
+    }
+
+    private fun getStatistics() = viewModelScope.launch {
+         myRepository.getUserStatistics().collectLatest {
+             _userStatistics.value = it
+         }
     }
 }
